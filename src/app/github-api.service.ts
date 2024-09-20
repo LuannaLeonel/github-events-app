defa8a1ae10fdb73e9856e73ed10b90ea99671f8
LuannaLeonel <luanna.silva@ccc.ufcg.edu.br>
2024-09-20 17:51:43 -0300

    untested class

--- src/app/github-api.service.ts
+++ src/app/github-api.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  fetchPublicEvents(): Observable<any[]> {
+    return this.http.get('https://api.github.com/events') as Observable<any[]> ;
+  }
+
+  fetchRepositoryEvents(owner: string, repo: string): Observable<any[]> {
+    return this.http.get(`https://api.github.com/repos/<span class="math-inline">\{owner\}/</span>{repo}/events`) as Observable<any[]>;
+  }
 }
