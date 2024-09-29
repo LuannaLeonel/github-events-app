cb5b5b0befa6c95d2dc9672f42ce319e4e05d870
joaopedrocporto <jpcamposporto@gmail.com>
2024-09-29 07:17:54 -0300

    new functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { distinct } from '../utils';
 import { GithubEventsService } from './github-events.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
   publicEvents: any[] = [];
   repoEvents: any[] = [];
+  title ='github-events-app'
 
   constructor(private githubEventsService: GithubEventsService) {}
 
@@ -48,5 +50,21 @@ export class AppComponent implements OnInit {
   }
   countRepoEventsByType(eventType: string): number {
     return this.repoEvents.filter(event => event.type === eventType).length;
+  }
+  filterDistinctPublicEventsByType(eventType: string, attribute: keyof any): any[] {
+    const filteredEvents = this.publicEvents.filter(event => event.type === eventType);
+    return distinct(filteredEvents, attribute);
+
+  }
+
+  filterDistinctEventsByType(eventType: string, attribute: keyof any): any[] {
+    const filteredEvents = this.publicEvents.filter(event => event.type === eventType);
+    return distinct(filteredEvents, attribute);
+  }
+
+
+  countDistinctRepoEventsByType(eventType: string, attribute: keyof any): number {
+    const filteredEvents = this.repoEvents.filter(event => event.type === eventType);
+    return distinct(filteredEvents, attribute).length;
   }
 }
