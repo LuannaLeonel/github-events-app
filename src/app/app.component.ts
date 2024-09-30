53a1491fe1beee89e376a8ef64631878eb1602ff
joaopedrocporto <jpcamposporto@gmail.com>
2024-09-30 19:49:50 -0300

    first front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Importar FormsModule
 import { distinct } from '../utils';
 import { GithubEventsService } from './github-events.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.scss'],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // Adicione FormsModule aqui
 })
 export class AppComponent implements OnInit {
   publicEvents: any[] = [];
   repoEvents: any[] = [];
-  title ='github-events-app'
+  filteredPublicEvents: any[] = [];
+  repoEventCount: number = 0;
+  title = 'github-events-app';
+  selectedEventType: string = ''; // Para armazenar o tipo de eve
+  eventTypes: string[] = [];
 
   constructor(private githubEventsService: GithubEventsService) {}
 
   ngOnInit(): void {
-    this.loadPublicEvents();
-    this.loadRepoEvents();
+    this.loadPublicEvents()
+    this.loadRepoEvents()
   }
 
   loadPublicEvents(): void {
     this.githubEventsService.getPublicEvents().subscribe(
       (data) => {
         this.publicEvents = data;
+        this.eventTypes = this.getDistinctEventTypes(); // Obt
       },
       (error) => {
         console.error('Erro ao carregar eventos públicos:', error);
@@ -36,35 +45,39 @@ export class AppComponent implements OnInit {
         this.repoEvents = data;
       },
       (error) => {
-        console.error(`Erro ao carregar eventos do repositório`, error);
+        console.error('Erro ao carregar eventos do repositório', error);
       }
     );
   }
 
+  getDistinctEventTypes(): string[] {
+    return [...new Set(this.publicEvents.map(event => event.type))];
+  }
 
-  filterEventsByType(eventType: string): any[] {
-    return this.publicEvents.filter(event => event.type === eventType);
+  filterEvents(): void {
+    if (this.selectedEventType) {
+      this.filteredPublicEvents = this.publicEvents.filter(event => event.type === this.selectedEventType);
+    } else {
+      this.filteredPublicEvents = this.publicEvents; // Reseta o filtro se não houver seleção
+    }
   }
-  filterRepoEventsByType(eventType: string): any[] {
-    return this.repoEvents.filter(event => event.type === eventType);
-  }
+
   countRepoEventsByType(eventType: string): number {
-    return this.repoEvents.filter(event => event.type === eventType).length;
+    return this.repoEvents.filter((event) => event.type === eventType).length;
   }
-  filterDistinctPublicEventsByType(eventType: string, attribute: keyof any): any[] {
-    const filteredEvents = this.publicEvents.filter(event => event.type === eventType);
-    return distinct(filteredEvents, attribute);
 
+  filterDistinctPublicEventsByType(eventType: string, attribute: keyof any): any[] {
+    const filteredEvents = this.publicEvents.filter((event) => event.type === eventType);
+    return distinct(filteredEvents, attribute);
   }
 
   filterDistinctEventsByType(eventType: string, attribute: keyof any): any[] {
-    const filteredEvents = this.publicEvents.filter(event => event.type === eventType);
+    const filteredEvents = this.publicEvents.filter((event) => event.type === eventType);
     return distinct(filteredEvents, attribute);
   }
 
-
   countDistinctRepoEventsByType(eventType: string, attribute: keyof any): number {
-    const filteredEvents = this.repoEvents.filter(event => event.type === eventType);
+    const filteredEvents = this.repoEvents.filter((event) => event.type === eventType);
     return distinct(filteredEvents, attribute).length;
   }
 }
